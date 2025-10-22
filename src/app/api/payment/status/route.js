dc69f579ab40f436051a748caef9489fcef8a62f
Arpit <aurpitaurpit@gmail.com>
2025-10-22 08:04:38 +0530

    Production

--- src/app/api/payment/status/route.js
+++ src/app/api/payment/status/route.js
@@ -9,7 +9,7 @@ import { Env, StandardCheckoutClient } from "pg-sdk-node";
 const clientId = process.env.PHONE_PE_CLIENT_ID;
 const clinetSecret = process.env.PHONE_PE_CLIENT_SECRET;
 const clientVersion = process.env.PHONE_PE_CLIENT_VERSION;
-const env = Env.SANDBOX;
+const env = Env.PRODUCTION;
 const client = StandardCheckoutClient.getInstance(
   clientId,
   clinetSecret,
@@ -62,3 +62,4 @@ const handler = async (req) => {
 
 export const GET = asyncHandler(handler);
 export const POST = asyncHandler(handler);
+
