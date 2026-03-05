e566b12eb491cfecbb7bd088c1d241802e8903c5
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2026-03-05 14:11:34 +0530

    fix clarity

--- src/components/clearity.jsx
+++ src/components/clearity.jsx
@@ -1,21 +1,24 @@
+"use client";
+
 import Script from "next/script";
 
 export default function ClarityComponent() {
   if (process.env.NODE_ENV !== "production") return null;
 
   return (
-    <Script
-      id="clarity"
-      strategy="lazyOnload"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "vkw299o6bb");
-        `,
-      }}
-    />
+    <Script type="text/javascript">
+      {(function (c, l, a, r, i, t, y) {
+        c[a] =
+          c[a] ||
+          function () {
+            (c[a].q = c[a].q || []).push(arguments);
+          };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", "vkw299o6bb")}
+    </Script>
   );
 }
