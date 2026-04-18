66b34b8ad4a8d76c0bd0197095088bb156b18835
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2026-04-18 10:08:39 +0530

    add redis

--- src/modules/coupon/services/getAllCoupon.js
+++ src/modules/coupon/services/getAllCoupon.js
@@ -1,16 +1,23 @@
 import { apiError, apiResponse, requiredAuth } from "@/shared";
 import Coupon from "../models/coupon";
 import { NextResponse } from "next/server";
+import { redis } from "@/shared/utils/Redis";
 
 export const getAllCoupon = async () => {
   const session = await requiredAuth();
   if (session?.user.role !== "admin") {
     throw new apiError(401, "unauthorized access");
   }
+  const cachedKey = `allcoupon`;
+  const cached = await redis.get(cachedKey);
+  if (cached) {
+    return JSON.parse(cached);
+  }
   const coupons = await Coupon.find();
   if (!coupons || coupons.length === 0) {
     throw new apiError(400, " coupons not found");
   }
+  await redis.set(cachedKey, JSON.stringify(coupons), "EX", 1200);
   return NextResponse.json(new apiResponse(200, "Coupons fetched successfully", coupons), {
     status: 200,
   });
--- src/modules/feedback/services/getFeedback.js
+++ src/modules/feedback/services/getFeedback.js
@@ -1,18 +1,24 @@
 import { apiError, apiResponse, requiredAuth } from "@/shared";
 import Feedback from "../model/feedback.model";
 import { NextResponse } from "next/server";
+import { redis } from "@/shared/utils/Redis";
 
 export const getFeedback = async () => {
   const session = await requiredAuth();
+  const cacheKey = `feedbacks`;
 
   if (session.user.role !== "admin") {
     throw new apiError(403, "Unauthorized access");
   }
-
+  const cached = await redis.get(cacheKey);
+  if (cached) {
+    return JSON.parse(cached);
+  }
   const feedback = await Feedback.find({})
     .populate("userId", "name email")
     .sort({ createdAt: -1 })
     .limit(50);
 
+  await redis.set(cacheKey, JSON.stringify(feedback), "EX", 1200);
   return NextResponse.json(new apiResponse(200, "Feedback retrieved successfully", feedback));
 };
