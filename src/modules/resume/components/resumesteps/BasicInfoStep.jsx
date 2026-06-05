fa2a2e547758a29bbcd665acef899c69e9432d7b
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2026-06-05 11:39:35 +0530

    made js collapable in basic ifo

--- src/modules/resume/components/resumesteps/BasicInfoStep.jsx
+++ src/modules/resume/components/resumesteps/BasicInfoStep.jsx
@@ -12,13 +12,18 @@ import {
 import { Input } from "@/shared/components/ui/input";
 import { Textarea } from "@/shared/components/ui/textarea";
 import { Card, CardContent } from "@/shared/components/ui/card";
-import { Linkedin, Github, Globe, ArrowRight } from "lucide-react";
+import { Linkedin, Github, Globe, ArrowRight, ChevronDown, Plus } from "lucide-react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/shared/components/ui/button";
 import React, { useEffect, useState } from "react";
 import useResumeStore from "@/store/useResumeStore";
 import { useRouter } from "next/navigation";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/shared/components/ui/collapsible";
 
 const schema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
@@ -36,6 +41,8 @@ const schema = z.object({
 const BasicInfoStep = () => {
   const formData = useResumeStore(s => s.formData);
   const updateForm = useResumeStore(s => s.updateForm);
+  const [openJd, setOpenJd] = useState(false);
+  const [openSocial, setOpenSocail] = useState(false);
   const [isloading, setIsLoading] = useState(false);
   const router = useRouter();
 
@@ -141,42 +148,6 @@ const BasicInfoStep = () => {
                     />
                   </div>
 
-                  <FormField
-                    name="jobDescription"
-                    control={form.control}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="text-slate-700 font-semibold text-xs md:text-sm">
-                          Target Job Description{" "}
-                          <span className="text-slate-400 font-normal">(Optional)</span>
-                        </FormLabel>
-
-                        <FormControl>
-                          <Textarea
-                            rows={5}
-                            placeholder="Paste only Requirements and Responsibilities.
-                              Example:
-                              • React.js, TypeScript, Redux
-                              • Build responsive UI components
-                              • Integrate REST APIs
-                              • Collaborate with backend developers
-
-                              Avoid company overview, perks, salary, and benefits."
-                            {...field}
-                            className="bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all resize-none text-xs md:text-sm placeholder:text-[10px] md:placeholder:text-sm"
-                          />
-                        </FormControl>
-
-                        <p className="text-[10px] md:text-xs text-slate-400">
-                          AI can use this to align your summary, skills, and resume bullets with the
-                          target job.
-                        </p>
-
-                        <FormMessage className="text-[10px]" />
-                      </FormItem>
-                    )}
-                  />
-
                   <div className="grid md:grid-cols-2 gap-4 md:gap-5">
                     <FormField
                       name="email"
@@ -239,72 +210,137 @@ const BasicInfoStep = () => {
                       </FormItem>
                     )}
                   />
-
-                  <div className="pt-4 border-t border-slate-100">
-                    <h3 className="text-[10px] md:text-sm font-semibold text-slate-900 mb-4 flex items-center gap-2 uppercase tracking-wider">
-                      <Globe className="w-3 md:w-4 h-3 md:h-4 text-indigo-500" />
-                      Social Links (Optional)
-                    </h3>
-
-                    <div className="space-y-3 md:space-y-4" id="tour-social-links">
+                  <Collapsible open={openJd} onOpenChange={setOpenJd}>
+                    <CollapsibleTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="w-full justify-between px-0 p-4 font-semibold"
+                      >
+                        <span className="text-sm">✨ Optimize Resume for a Job (Optional)</span>
+                        <Plus
+                          className={`h-6 w-6  font-bold transition-transform ${openJd ? "rotate-180" : ""}`}
+                        />
+                      </Button>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
                       <FormField
-                        name="linkedin"
+                        name="jobDescription"
                         control={form.control}
                         render={({ field }) => (
                           <FormItem>
-                            <FormControl>
-                              <div className="relative">
-                                <Linkedin className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
-                                <Input
-                                  placeholder="LinkedIn URL"
-                                  {...field}
-                                  className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
-                                />
-                              </div>
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      />
+                            <FormLabel className="text-slate-700 font-semibold text-xs md:text-sm">
+                              Target Job Description{" "}
+                              <span className="text-slate-400 font-normal">(Optional)</span>
+                            </FormLabel>
 
-                      <FormField
-                        name="github"
-                        control={form.control}
-                        render={({ field }) => (
-                          <FormItem>
                             <FormControl>
-                              <div className="relative">
-                                <Github className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
-                                <Input
-                                  placeholder="GitHub URL"
-                                  {...field}
-                                  className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
-                                />
-                              </div>
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      />
+                              <Textarea
+                                rows={5}
+                                placeholder="Paste only Requirements and Responsibilities.
+                              Example:
+                              • React.js, TypeScript, Redux
+                              • Build responsive UI components
+                              • Integrate REST APIs
+                              • Collaborate with backend developers
 
-                      <FormField
-                        name="portfolio"
-                        control={form.control}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormControl>
-                              <div className="relative">
-                                <Globe className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
-                                <Input
-                                  placeholder="Portfolio URL"
-                                  {...field}
-                                  className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
-                                />
-                              </div>
+                              Avoid company overview, perks, salary, and benefits."
+                                {...field}
+                                className="bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all resize-none text-xs md:text-sm placeholder:text-[10px] md:placeholder:text-sm"
+                              />
                             </FormControl>
+
+                            <p className="text-[10px] md:text-xs text-slate-400">
+                              AI can use this to align your summary, skills, and resume bullets with
+                              the target job.
+                            </p>
+
+                            <FormMessage className="text-[10px]" />
                           </FormItem>
                         )}
                       />
-                    </div>
-                  </div>
+                    </CollapsibleContent>
+                  </Collapsible>
+                  <Collapsible open={openSocial} onOpenChange={setOpenSocail}>
+                    <CollapsibleTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="w-full justify-between px-0 p-4 font-semibold"
+                      >
+                        <span className="text-sm">✨ Social Links (Optional)</span>
+                        <Plus
+                          className={`h-6 w-6  font-bold transition-transform ${openJd ? "rotate-180" : ""}`}
+                        />
+                      </Button>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      {" "}
+                      <div className="pt-4 border-t border-slate-100">
+                        <h3 className="text-[10px] md:text-sm font-semibold text-slate-900 mb-4 flex items-center gap-2 uppercase tracking-wider">
+                          <Globe className="w-3 md:w-4 h-3 md:h-4 text-indigo-500" />
+                          Social Links (Optional)
+                        </h3>
+
+                        <div className="space-y-3 md:space-y-4" id="tour-social-links">
+                          <FormField
+                            name="linkedin"
+                            control={form.control}
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  <div className="relative">
+                                    <Linkedin className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
+                                    <Input
+                                      placeholder="LinkedIn URL"
+                                      {...field}
+                                      className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
+                                    />
+                                  </div>
+                                </FormControl>
+                              </FormItem>
+                            )}
+                          />
+
+                          <FormField
+                            name="github"
+                            control={form.control}
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  <div className="relative">
+                                    <Github className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
+                                    <Input
+                                      placeholder="GitHub URL"
+                                      {...field}
+                                      className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
+                                    />
+                                  </div>
+                                </FormControl>
+                              </FormItem>
+                            )}
+                          />
+
+                          <FormField
+                            name="portfolio"
+                            control={form.control}
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  <div className="relative">
+                                    <Globe className="absolute left-3 top-2.5 md:top-3 w-4 md:w-5 h-4 md:h-5 text-slate-400" />
+                                    <Input
+                                      placeholder="Portfolio URL"
+                                      {...field}
+                                      className="pl-9 md:pl-10 bg-slate-50 border-slate-200 focus:bg-white focus:border-indigo-500 transition-all h-8 md:h-11 text-[10px] md:text-sm"
+                                    />
+                                  </div>
+                                </FormControl>
+                              </FormItem>
+                            )}
+                          />
+                        </div>
+                      </div>
+                    </CollapsibleContent>
+                  </Collapsible>
 
                   <div className="pt-4 flex justify-end">
                     <Button
