f52dac901943dad0ba827f619cf6bc12bbd9aae8
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2026-06-12 15:07:37 +0530

    perf: separate logic of ats sore checker into client and api

--- src/shared/utils/ReqireAuth.js
+++ src/shared/utils/ReqireAuth.js
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { authOptions } from "@/modules/auth/services/options";
+import authOptions from "@/modules/auth/services/options";
 import { getServerSession } from "next-auth";
 import apiError from "./apiError";
 
