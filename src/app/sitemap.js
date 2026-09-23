4bb4d78e6b1be0f04d3bd2c8ef9d263ec7b1d5b7
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2026-09-23 13:39:58 +0530

    fix site map

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,7 @@
 // app/sitemap.js
-import { client } from "@/sanity";
 import seoPages from "./(landingPage)/seo-pages.json";
 import careerPages from "./(landingPage)/career-pages.json";
-
+import { client } from "@/sanity";
 export default async function sitemap() {
   const baseUrl = "https://www.nextcv.in";
 
@@ -42,7 +41,7 @@ export default async function sitemap() {
   }));
 
   const careerPage = careerPages.map(page => ({
-    url: `${baseUrl}/${page.slug}`,
+    url: `${baseUrl}/career/${page.slug}`,
     lastModified: staticLastModified,
     changeFrequency: "monthly",
     priority: 0.85,
@@ -54,6 +53,8 @@ export default async function sitemap() {
     changeFrequency: "weekly",
     priority: 0.75,
   }));
+
+  console.log(careerPage);
 
   return [...staticPages, ...careerPage, ...dynamicSeoPages, ...blogPages];
 }
